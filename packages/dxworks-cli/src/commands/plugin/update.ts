42175cf15a6d0f80e602477a616912000ab2a529
MarioRivis <mario.rivis@gmail.com>
2021-11-03 01:14:55 +0200

    fixed upgrade --latest command

--- packages/dxworks-cli/src/commands/plugin/update.ts
+++ packages/dxworks-cli/src/commands/plugin/update.ts
@@ -1,7 +1,7 @@
 import {Command} from 'commander'
 import {npm} from '../../npm'
 import {execSync} from 'child_process'
-import {ncuPath} from '../../utils'
+import {ncuPath, pluginsFolder} from '../../utils'
 
 export const pluginUpdate = new Command()
   .name('update')
@@ -14,7 +14,7 @@ export const pluginUpdate = new Command()
 async function updatePlugins(plugins: string[], options: any) {
   if (!plugins || plugins.length == 0) {
     if (options.latest) {
-      execSync(`${ncuPath} -u`)
+      execSync(`${ncuPath} -u`, {cwd:pluginsFolder, stdio:'inherit'})
       npm.install()
     } else {
       npm.update()
